471470ff2eb940f89e3be1bae85c843a226ce2a0
Sourav <sv2953987@gmail.com>
2026-06-06 05:11:48 -0700

    chore: remove redundant files pulled from remote

--- teampulse-dashboard/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // We can add rewrites if we want to proxy the backend,
-  // but the code uses direct URLs for SSE.
-};
-
-module.exports = nextConfig;
